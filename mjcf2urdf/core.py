2e2fc4642d7f5cbf7591263c02979431c728090d
Junyao Shi <jackshi1999@gmail.com>
2020-07-14 21:16:26 -0400

    changed visual to true and added usage of new urdfeditor

--- mjcf2urdf/core.py
+++ mjcf2urdf/core.py
@@ -1,7 +1,8 @@
 import os.path as osp
 
 import pybullet_utils.bullet_client as bulllet_client
-import pybullet_utils.urdfEditor as urdfEditor
+# import pybullet_utils.urdfEditor as urdfEditor
+import mjcf2urdf.urdfEditor as urdfEditor
 
 import mjcf2urdf
 
@@ -17,19 +18,20 @@ def convert_mjcf_to_urdf(input_mjcf, output_path):
         output directory path of urdf.
     """
     client = bulllet_client.BulletClient()
-    objs = client.loadMJCF(
-        input_mjcf, flags=client.URDF_USE_IMPLICIT_CYLINDER)
+    # objs = client.loadMJCF(
+    #     input_mjcf, flags=client.URDF_USE_IMPLICIT_CYLINDER)
+    objs = client.loadMJCF(input_mjcf)
 
     # create output directory
     mjcf2urdf.makedirs(output_path)
 
     for obj in objs:
         humanoid = objs[obj]
-        ue = urdfEditor.UrdfEditor()
+        ue = urdfEditor.UrdfEditorNew()
         ue.initializeFromBulletBody(humanoid, client._client)
         robot_name = str(client.getBodyInfo(obj)[1], 'utf-8')
         part_name = str(client.getBodyInfo(obj)[0], 'utf-8')
-        save_visuals = False
+        save_visuals = True
         outpath = osp.join(
             output_path, "{}_{}.urdf".format(robot_name, part_name))
         ue.saveUrdf(outpath, save_visuals)
